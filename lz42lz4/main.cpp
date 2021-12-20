916a0f6ca018dfb4c864c35953d23c725991058a
David Riusech <v1X3Q0@gmail.com>
2021-12-19 23:21:07 -0500

    raw and orig added

--- lz42lz4/main.cpp
+++ lz42lz4/main.cpp
@@ -4,6 +4,9 @@
 #include <stdint.h>
 #include <vector>
 #include <string>
+#include <libgen.h>
+#include <linux/limits.h>
+#include <string.h>
 
 #include <lz4.h>
 #include <lz4frame.h>
@@ -33,8 +36,9 @@ typedef struct
     size_t out_sz;
 } decomp_save;
 
-void iterate_lz4(char* lz4_in_raw, size_t lz4_in_sz)
+int iterate_lz4(char* lz4_in_raw, size_t lz4_in_sz, size_t* lz4_out_sz)
 {
+    int result = -1;
     LZ4F_dctx *curCtx = 0;
     LZ4F_frameInfo_t curFrame;
     size_t lz4_head_sz = lz4_in_sz;
@@ -68,18 +72,26 @@ void iterate_lz4(char* lz4_in_raw, size_t lz4_in_sz)
     post_lz4 = (void*)cur_decomp_sz;
     i = ((size_t)cur_decomp_sz - (size_t)lz4_in_raw);
 
-    for (; i < lz4_in_sz; i++)
+    if (i == lz4_in_sz)
     {
-        if (lz4_in_raw[i] != 0)
-        {
-            printf("found nonzero block at offset 0x%x\n", i);
-            break;
-        }
+        printf("input lz4 has no data post final checksum\n");
+        goto fail;
     }
 
+    *lz4_out_sz = i;
+    // for (; i < lz4_in_sz; i++)
+    // {
+    //     if (lz4_in_raw[i] != 0)
+    //     {
+    //         printf("found nonzero block at offset 0x%x\n", i);
+    //         break;
+    //     }
+    // }
+
+    result = 0;
 fail:
     SAFE_LZ4FREE(curCtx);
-    return;
+    return result;
 }
 
 int iterate_decomp_lz4(void* lz4_in_raw, size_t lz4_in_sz, void** out_payload, size_t* out_sz)
@@ -150,12 +162,16 @@ int main(int argc, char **argv)
     size_t lz4_in_sz = 0;
     size_t lz4_out_sz = 0;
     
-    std::string out_file_str;
-    const char* dec_out_name = 0;
-    FILE* out_file;
-    std::vector<decomp_save*> decomp_array;
+    int lz4_rip = 0;
+    int dec_it = 0;
 
-    while ((opt = getopt(argc, argv, "i:")) != -1)
+    char dup_path[PATH_MAX];
+    std::string out_file_suffix = "";
+    std::string out_file_str;
+    const char* out_file_name = 0;
+    FILE* out_file;
+
+    while ((opt = getopt(argc, argv, "dri:o:")) != -1)
     {
         switch (opt)
         {
@@ -163,7 +179,13 @@ int main(int argc, char **argv)
             lz4_in_name = optarg;
             break;
         case 'o':
-            dec_out_name = optarg;
+            out_file_name = optarg;
+            break;
+        case 'r':
+            lz4_rip = 1;
+            break;
+        case 'd':
+            dec_it = 1;
             break;
         default: /* '?' */
             usage(argv[0]);
@@ -173,19 +195,39 @@ int main(int argc, char **argv)
     SAFE_BAIL(block_grab(lz4_in_name, (void**)&lz4_in_raw, &lz4_in_sz) == -1);
     lz4_head_sz = lz4_in_sz;
 
-    iterate_lz4(lz4_in_raw, lz4_in_sz);
+    if (lz4_rip)
+    {
+        SAFE_BAIL(iterate_lz4(lz4_in_raw, lz4_in_sz, &lz4_out_sz) == -1);
+        lz4_out_raw = lz4_in_raw;
+        out_file_suffix = ".orig";
+        
+        strcpy(dup_path, lz4_in_name);
+        dirname(dup_path);
+        out_file_str = (const char*)dup_path;
+        out_file_str += "/excess_dump.raw";
+        out_file = fopen(out_file_str.data(), "w");
+        fwrite((lz4_out_raw + lz4_out_sz), 1, lz4_in_sz - lz4_out_sz, out_file);
+        SAFE_FCLOSE(out_file);
+    }
+    else if (dec_it)
+    {
+        SAFE_BAIL(iterate_decomp_lz4(lz4_in_raw, lz4_in_sz, (void**)&lz4_out_raw, &lz4_out_sz) == -1);
+        out_file_suffix = ".raw";
+    }
 
-    iterate_decomp_lz4(lz4_in_raw, lz4_in_sz, (void**)&lz4_out_raw, &lz4_out_sz);
-    // write payload to file
-    if (dec_out_name == 0)
+    if (out_file_name == 0)
     {
         out_file_str = lz4_in_name;
-        out_file_str += ".raw";
-        dec_out_name = out_file_str.data();
-    }
-    out_file = fopen(dec_out_name, "r");
+        out_file_str += out_file_suffix;
+        out_file_name = out_file_str.data();
+    }        
+
+    // write payload to file
+    out_file = fopen(out_file_name, "w");
     fwrite(lz4_out_raw, 1, lz4_out_sz, out_file);
 
 fail:
+    SAFE_FREE(lz4_in_raw);
+    SAFE_FCLOSE(out_file);
     return 0;
 }
